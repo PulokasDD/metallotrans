b876e05b9af051d9383774da8c47b6b23f14adfe
Ilya <iliakrapivkin@gmail.com>
2020-10-29 13:58:42 +0300

    helper to HBS

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import logger from 'morgan';
 import mongoose from 'mongoose';
 import session from 'express-session';
 import sha256 from 'sha256';
+import Handlebars from 'hbs';
 
 import indexRouter from './routes/index.js';
 import priceRouter from './routes/price.js';
@@ -12,7 +13,6 @@ import adminRouter from './routes/admin.js';
 
 // import Admin from './models/admin';
 // import Customer from './models/customer';
-import Product from './models/product.js';
 
 const app = express();
 
@@ -23,6 +23,8 @@ mongoose.connect('mongodb://localhost:27017/metallotrans', {
 
 // view engine setup
 app.set('view engine', 'hbs');
+
+Handlebars.registerHelper('inc', (val) => val + 1);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -36,7 +38,7 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false, maxAge: 1000 * 60 * 30 },
-  })
+  }),
 );
 
 app.use((req, res, next) => {
@@ -51,17 +53,10 @@ app.use('/admin', adminRouter);
 
 const port = process.env.port || 3000;
 // CODE:
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/show', async (req, res) => {
-  const data = await Product.find();
-  res.send(data);
-});
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
 
 export default app;
+// npm i -S handlebars
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
+/* eslint-disable import/extensions */
 import express from 'express';
+import Product from '../models/product.js';
 
 const router = express.Router();
-
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { title: 'index page' });
+router.get('/', async (req, res) => {
+  const data = await Product.find();
+  res.render('index', { data });
 });
 
 export default router;
